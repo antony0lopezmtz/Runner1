beb6f227d92bb3d53ce4aa8394bbb3a556e7ce03
antony0lopezmtz <antony0lopezmtz@gmail.com>
2019-02-01 20:02:27 +0100

    Generacion Numeros Plataformas 2

--- Assets/Scripts/criptsJuego/Scripts/EscenaJuego/Controladores/PlataformaCarga.cs
+++ Assets/Scripts/criptsJuego/Scripts/EscenaJuego/Controladores/PlataformaCarga.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlataformaCarga : MonoBehaviour {
 
 	List<GameObject> lista;
-	public 
+	public int sizeOfList;
 
 	// Use this for initialization
 	void Start () {
 
 		GameObject Random = GameObject.Find("MapController");
 		MapController GeneracionPlataformas = Random.GetComponent<MapController>();
-		var sizeOfList = GeneracionPlataformas.mapa;
+		lista = GeneracionPlataformas.mapa;
+		sizeOfList = lista.Count;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	private void OnTriggerEnter(Collider Player)
+	{
+		Debug.Log(Random.Range(0, sizeOfList));
 	}
 }
