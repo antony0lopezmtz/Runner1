e4d30e4f6279f570e57da6af03dee24adf11261b
antony0lopezmtz <antony0lopezmtz@gmail.com>
2019-03-01 20:08:20 +0100

    Intento de generacion procedural

---
+++ Assets/Scripts/criptsJuego/Scripts/EscenaJuego/Controladores/ColisionPlataforma.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionPlataforma : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			var script_mapa = GameObject.Find("MapController").GetComponent<MapController>();
+			int numeroAleatorio = Random.Range(0, script_mapa.mapa.Count);
+			var elementoAleatorio = script_mapa.mapa[numeroAleatorio];
+			var puntoFinal = script_mapa.elementoActual.transform.GetChild(0);
+			script_mapa.elementoActual = Instantiate(elementoAleatorio, puntoFinal.position, puntoFinal.rotation);
+			Debug.Log("NuevaP");
+		}
+		
+	}
+}
--- Assets/Scripts/criptsJuego/Scripts/EscenaJuego/Controladores/MapController.cs
+++ Assets/Scripts/criptsJuego/Scripts/EscenaJuego/Controladores/MapController.cs
@@ -2,39 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapController : ControladorBasico {
+public class MapController : ControladorBasico
+{
+
 	public GameObject elementoInicial;
+
 	public Transform puntoInicial;
-	public GameObject elementoSiguiente;
-	public Transform puntoSiguiente;
-	public int sizeOflist;
 
-	public List<GameObject> mapa = new List<GameObject>();
+	public GameObject elementoActual;
 
-
+	public List<GameObject> mapa;
 	// Use this for initialization
-
-	void Start() {
-
+	void Start()
+	{
 		base.Start();
-
-		sizeOflist = mapa.Count;
-
+		mapa = new List<GameObject>();
+		encontrarPlataformas();
+		//                      GameObject              Position                        Rotation
 		var plataformaInicio = Instantiate(elementoInicial, puntoInicial.position, puntoInicial.rotation);
-		var destino = plataformaInicio.gameObject.transform.Find("puntoFinal");
-		Instantiate(elementoSiguiente, destino.position, destino.rotation);
+		Transform destino = plataformaInicio.gameObject.transform.Find("puntoFinal");
+		int numeroAleatorio = Random.Range(0, mapa.Count);
+		Debug.Log(numeroAleatorio);
+		var elementoAleatorio = mapa[numeroAleatorio];
+		Debug.Log(elementoAleatorio);
+		elementoActual = Instantiate(elementoAleatorio, destino.position, destino.rotation);
+	}
 
-		Debug.Log(Random.Range(0, sizeOflist));
-
+	void encontrarPlataformas()
+	{
+		var plataforma1 = GameObject.Find("Pasillo");
+		mapa.Add(plataforma1);
+		var plataforma2 = GameObject.Find("saltoEntero");
+		mapa.Add(plataforma2);
+		var plataforma3 = GameObject.Find("saltoDerecha");
+		mapa.Add(plataforma3);
+		var plataforma4 = GameObject.Find("saltoIzquierda");
+		mapa.Add(plataforma1);
+		var plataforma5 = GameObject.Find("paredIzquierda");
+		mapa.Add(plataforma2);
+		var plataforma6 = GameObject.Find("paredDerecha");
+		mapa.Add(plataforma3);
+		var plataforma7 = GameObject.Find("agachardeDerecha");
+		mapa.Add(plataforma1);
+		var plataforma8 = GameObject.Find("agacharseIzquierda");
+		mapa.Add(plataforma2);
+		var plataforma9 = GameObject.Find("agacharseMedio");
+		mapa.Add(plataforma3);
+		var plataforma10 = GameObject.Find("pasilloMedio");
+		mapa.Add(plataforma2);
+		var plataforma11 = GameObject.Find("pasilloMedioSalto");
+		mapa.Add(plataforma3);
 	}
 
 	// Update is called once per frame
-
-	void Update() {
-
-
+	void Update()
+	{
 
 	}
+
 	public override void juegoEnPausa()
 	{
 
@@ -44,6 +69,4 @@ public class MapController : ControladorBasico {
 	{
 
 	}
-
-
 }
