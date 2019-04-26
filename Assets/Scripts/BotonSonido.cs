cb8651bc8fa985295d8cb96b3af9de544a24506b
antony0lopezmtz <antony0lopezmtz@gmail.com>
2019-04-26 19:08:34 +0200

    SoundButton / Custom Cursor

---
+++ Assets/Scripts/BotonSonido.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonSonido : MonoBehaviour {
+
+	public AudioClip beep;
+	public AudioSource beepSource;
+	public Button btn1;
+	public Button btn2;
+	public Button btn3;
+	public Button btn4;
+	public Button btn5;
+
+	// Use this for initialization
+	void Start () {
+
+		beepSource.clip = beep;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		btn1.onClick.AddListener(PlaySound);
+		btn2.onClick.AddListener(PlaySound);
+		btn3.onClick.AddListener(PlaySound);
+		btn4.onClick.AddListener(PlaySound);
+		btn5.onClick.AddListener(PlaySound);
+
+	}
+
+	void PlaySound()
+	{
+		beepSource.Play();
+	}
+	
+}
--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -5,30 +5,20 @@ using UnityEngine;
 
 public class Cursor : MonoBehaviour {
 
-	private static void SetCursor(Texture2D texture, Vector2 hotspot, CursorMode cursorMode)
-	{
-
-	}
 	public Texture2D cursorTexture;
 	public CursorMode cursorMode = CursorMode.Auto;
 	public Vector2 hotSpot = Vector2.zero;
-	void OnMouseEnter()
-	{
-		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-	}
 
-	void OnMouseExit()
+	public static void SetCursor(Texture2D texture, Vector2 hotspot, CursorMode cursorMode)
 	{
-		Cursor.SetCursor(null, Vector2.zero, cursorMode);
-	}
-	// Use this for initialization
-	void Start () {
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+
 	}
 
 }
